6f56a48d95bc90802208070084463a0e93b74d65
Hongkiat_clone <iamnabeel528@gmail.com>
2026-01-19 02:02:21 +0500

    simple class

--- simple_class.py
+++ simple_class.py
@@ -1,8 +1,25 @@
 
-class MyClass:
+# class MyClass:
     
-    x = 10  # Class property
+#     x = 10  # Class property
 
-my_class = MyClass()
-property_value = my_class.x
-print(property_value)  
+
+# my_class = MyClass()
+# property_value = my_class.x
+# print(property_value)  
+
+
+# print(MyClass)
+
+
+class Juicer:
+    on = 'machine is on'    #class attribute
+    off = 'machine is off'
+    
+juicer_machine = Juicer()
+
+machine_on = juicer_machine.on
+machine_off = juicer_machine.off
+
+print(machine_on)
+print(machine_off)
